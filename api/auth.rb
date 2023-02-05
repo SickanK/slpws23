b727014ffa2e49a9636f7755656fce0826ceaafa
SickanK <sixtenklementsson@gmail.com>
2023-02-05 23:50:37 +0100

    lade till error handling på unique value conflicts i samband med registrering

--- api/auth.rb
+++ api/auth.rb
@@ -10,6 +10,9 @@ require_relative "../models/auth.rb"
 # Login
 
 get("/login") do
+  @errors = session.delete(:errors)
+  @values = session.delete(:values)
+
   slim(:login)
 end
 
@@ -17,10 +20,14 @@ post("/login") do
   email = params[:email].strip!
   password = params[:password].strip!
 
+  session[:errors] = {}
+  session[:values] = { :email => email, :password => password }
+
   # Check if fields are not empty
 
   if (email == "" || email == nil || password == "" || password == nil)
-    session[:error] = { "login" => "Du måste fylla i alla fält", fields: params }
+    session[:errors] = { "login" => "Du måste fylla i alla fält" }
+
     redirect("/")
   end
 
@@ -35,7 +42,7 @@ post("/login") do
 
     redirect("/")
   else
-    session[:error] = { "login" => "Fel användarnamn eller lösenord" }
+    session[:error] = { :login => "Fel användarnamn eller lösenord" }
     redirect("/login")
   end
 end
@@ -43,6 +50,9 @@ end
 # Signup
 
 get("/signup") do
+  @errors = session.delete(:errors)
+  @values = session.delete(:values)
+
   slim(:signup)
 end
 
@@ -51,30 +61,24 @@ post("/signup") do
   email = params[:email].strip
   password = params[:password].strip
 
+  session[:errors] = {}
+  session[:values] = { :name => name, :email => email, :password => password }
+
   # Check if user already exists
 
   if (name == "" || name == nil || email == "" || email == nil || password == "" || password == nil)
-    session[:error] = { "signup" => "Du måste fylla i alla fält", "fields" => {
-      "name" => name,
-      "email" => email,
-      "password" => password,
-    } }
+    session[:errors] = { :signup => "Du måste fylla i alla fält" }
+
     redirect("/signup")
   elsif (password.length < 8)
-    session[:error] = { "signup" => "Lösenordet måste vara minst 8 tecken", "fields" => {
-      "name" => name,
-      "email" => email,
-      "password" => "",
-
-    } }
+    session[:errors] = { :signup => "Lösenordet måste vara minst 8 tecken" }
+    session[:values][:password] = ""
 
     redirect("/signup")
   elsif (name.include? "@")
-    session[:error] = { "signup" => "Namnet får inte innehålla @-tecken", "fields" => {
-      "name" => name,
-      "email" => email,
-      "password" => password,
-    } }
+    session[:errors] = { :signup => "Namnet får inte innehålla @-tecken" }
+    session[:values][:password] = ""
+
     redirect("/signup")
   else
     session[:error] = nil
@@ -83,5 +87,22 @@ post("/signup") do
   # Add user to database
   password_digest = BCrypt::Password.create(password)
 
-  new_user(name, email, password_digest)
+  begin
+    new_user = new_user(name, email, password_digest)
+
+    session[:user_id] = new_user
+    redirect("/")
+  rescue Exception => e
+    if e.message.include? "name"
+      session[:errors] = { :name => "Användarnamnet är redan upptagen" }
+      session[:values][:name] = ""
+    end
+
+    if e.message.include? "email"
+      session[:errors] = { :email => "E-postadressen är redan upptagen" }
+      session[:values][:email] = ""
+    end
+
+    redirect("/signup")
+  end
 end
--- models/auth.rb
+++ models/auth.rb
@@ -1,13 +1,35 @@
 require "date"
 require_relative "../app.rb"
 
+def connect_to_db()
+  db = SQLite3::Database.new(DB_PATH)
+  db.results_as_hash = true
+  return db
+end
+
 def new_user(name, email, password)
   db = connect_to_db()
   db.results_as_hash = true
 
+  # Hash password
   created_at = Time.now.to_i
 
-  db.execute("INSERT INTO User (name, email, password_hash, created_at) VALUES (?, ?, ?, ?)", [name, email, password, created_at])
+  # Insert user into database
+  begin
+    db.execute("INSERT INTO User (name, email, password_hash, created_at) VALUES (?, ?, ?, ?)", [name, email, password, created_at])
+  rescue SQLite3::ConstraintException
+    conflict = []
+
+    if db.execute("SELECT COUNT(*) FROM User WHERE name = ?", [name]).first[0] > 0
+      conflict << "name"
+    end
+    if db.execute("SELECT COUNT(*) FROM User WHERE email = ?", [email]).first[0] > 0
+      conflict << "email"
+    end
+
+    db.close
+    raise Exception.new(conflict.join(", "))
+  end
 
   db.close
 end
